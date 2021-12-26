8b0c494513d0fa0a9b0c2a6ef7f6194f96d759a5
William R. Zwicky <wrzwicky@pobox.com>
2021-12-26 17:23:25 +0000

    even dumber roll command

--- cogs/dice.py
+++ cogs/dice.py
@@ -7,12 +7,34 @@ class DiceCog(commands.Cog, name="Totally random dice roller"):
     def __init__(self, bot):
         self.bot = bot
 
+        self.up = "abcdefghijklmnopqrstuvwxyz" + \
+                  "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + \
+                  "1234567890" + \
+                  "!@#$%^&*()-=_+" + \
+                  "[]\;',./{}|:\"<>?" + \
+                  ""
+        self.down = "ɐqɔpǝɟƃɥıɾʞןɯuodbɹsʇnʌʍxʎz" + \
+                    "∀qϽᗡƎℲפHIſʞ˥WNOԀὉᴚS⊥∩ΛMX⅄Z" + \
+                    "ƖᄅƐㄣϛ9ㄥ860" + \
+                    "¡@#$%^⅋*)(-=‾+" + \
+                    "][\\;,'˙/}{|:,><¿" + \
+                    ""
+
     @commands.command()
-    async def roll(self, ctx, word: str = None):
-      if not word:
+    async def roll(self, ctx, *words):
+      if not words:
         await ctx.message.add_reaction("\N{SHRUG}")
-      else:        
-        await ctx.channel.send("9")
+      else:
+        word = " ".join(words)
+        #s = "9"
+        s = "".join([self.derp(c) for c in word[::-1]])
+        await ctx.channel.send(s)
+
+    def derp(self, c):
+      try:
+        return self.down[self.up.index(c)]
+      except:
+        return c
 
 def setup(bot):
 	bot.add_cog(DiceCog(bot))
